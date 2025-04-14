358af5fd1c1958e2c17b5010542746f5af9ae619
travailbeyondexcellence <zen@paathshala.world>
2025-04-14 12:44:47 +0530

    Saving JS files

---
+++ Strings/1.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} name
+ */
+function shoutMyName(name) {
+    let uppercaseName = name.toUpperCase;
+    return uppercaseName; 
+    
+    }
+    
+    // Sample usage - do not modify
+    console.log(shoutMyName("Sam")); // "SAM"
+    console.log(shoutMyName("Charley")); // "CHARLEY"
+    console.log(shoutMyName("alex")); // "ALEX"
+    
