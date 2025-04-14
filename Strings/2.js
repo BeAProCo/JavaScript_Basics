1597a6658be0213580310a4557cc95af8a2d6ccc
travailbeyondexcellence <zen@paathshala.world>
2025-04-14 12:57:15 +0530

    using JS string functions

---
+++ Strings/2.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} name
+ */
+function lowerName(name) {
+
+}
+
+// Sample usage - do not modify
+console.log(lowerName("Sam")); // "sam"
+console.log(lowerName("ALEX")); // "alex"
